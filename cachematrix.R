c086033b662c40eb3baadf4ac3cec3650366cd34
Joseph <jbgonzalez81@gmail.com>
2014-10-26 08:46:26 -0400

    Programming Assignment 2
    First attempt

---
+++ cachematrix.R
@@ -0,0 +1,35 @@
+## This program attempts to cache a square matrix for future calculation.
+## The functions with store and retrieve the matrix and its inverse when called.
+
+## Store a square matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) m<<- solve
+  getmatrix<-function() m
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
+}
+
+
+
+
+## Solve the inverse of a matrix 
+
+cacheSolve <- function(x=matrix(), ...) {
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
+}
